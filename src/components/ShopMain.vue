abed71e453d32bbbaf03dd2822eab42523fe46e3
qingw <qingw@jumei.com>
2018-06-13 16:47:53 +0800

    1. 修改按钮的点击事件。

--- src/components/ShopMain.vue
+++ src/components/ShopMain.vue
@@ -16,11 +16,18 @@
         background-color="#409EFF"
         text-color="#fff"
         active-text-color="#E6A23C">
-        <el-menu-item index="1" @click.native="getApiData">电影排行榜</el-menu-item>
+        <el-menu-item index="1">电影排行榜</el-menu-item>
         <el-menu-item index="2">未知数据</el-menu-item>
       </el-menu>
 
-      <el-table :data="movieData">
+
+      <el-table
+        v-show="isMove"
+        v-loading="loading2"
+        element-loading-text="拼命加载中"
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.8)"
+        :data="movieData">
         <el-table-column prop="id" label="id" width="100" align="center">
         </el-table-column>
         <el-table-column prop="title" label="标题" width="500" align="center">
@@ -28,6 +35,10 @@
         <el-table-column prop="quote" label="评价" align="center">
         </el-table-column>
       </el-table>
+
+      <div v-show="!isMove">
+        测试页面！！！
+      </div>
 
     </el-main>
   </el-container>
@@ -52,12 +63,21 @@
         tableData: Array(3).fill(item),
         movieData: [],
         activeIndex: '1',
-        activeIndex2: '1'
+        activeIndex2: '1',
+        loading2: false,
+        isMove: true,
       };
     },
     methods: {
       handleSelect(key, keyPath) {
         // console.log(key, keyPath);
+        if (key == 1) {
+          this.isMove = true;
+          this.loading2 = true;
+          this.getApiData()
+        } else if (key == 2) {
+          this.isMove = false;
+        }
       },
       getApiData: function () {
         // var url = "http://zkteam.cc/Shop/show";
@@ -69,7 +89,8 @@
 
         this.$axios.get(url).then(function (result) {
           console.log(result);
-          that.movieData = result.data.result
+          that.movieData = result.data.result;
+          that.loading2 = false;
         });
       }
     }
