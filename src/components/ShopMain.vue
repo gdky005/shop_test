b058ce3969cb0a2482f6b24daea62e59dcdf530d
qingw <qingw@jumei.com>
2018-06-13 18:53:12 +0800

    1. 添加 shop 真实数据的展示。

--- src/components/ShopMain.vue
+++ src/components/ShopMain.vue
@@ -20,26 +20,71 @@
         <el-menu-item index="2">未知数据</el-menu-item>
       </el-menu>
 
-
-      <el-table
-        v-show="isMove"
-        v-loading="loading2"
-        element-loading-text="拼命加载中"
-        element-loading-spinner="el-icon-loading"
-        element-loading-background="rgba(0, 0, 0, 0.8)"
-        :data="movieData">
-        <el-table-column prop="id" label="id" width="100" align="center">
-        </el-table-column>
-        <el-table-column prop="title" label="标题" width="500" align="center">
-        </el-table-column>
-        <el-table-column prop="quote" label="评价" align="center">
-        </el-table-column>
-      </el-table>
-
-      <div v-show="!isMove">
-        测试页面！！！
+      <div id="newRow">
+        <el-table
+          :data="shopData"
+          style="width: 100%">
+          <el-table-column
+            label="id"
+            width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.id }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="名称"
+            width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>名称: {{ scope.row.name }}</p>
+                <p>PID: {{ scope.row.pid }}</p>
+                <p>价格: {{ scope.row.price }}</p>
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="处理">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row.pid)">编辑
+              </el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row.name)">删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
       </div>
 
+      <div v-show="false">
+
+        <el-table
+          v-show="isMove"
+          v-loading="loading2"
+          element-loading-text="拼命加载中"
+          element-loading-spinner="el-icon-loading"
+          element-loading-background="rgba(0, 0, 0, 0.8)"
+          :data="movieData">
+          <el-table-column prop="id" label="id" width="100" align="center">
+          </el-table-column>
+          <el-table-column prop="title" label="标题" width="500" align="center">
+          </el-table-column>
+          <el-table-column prop="quote" label="评价" align="center">
+          </el-table-column>
+        </el-table>
+
+        <div v-show="!isMove">
+          测试页面！！！
+        </div>
+
+
+      </div>
     </el-main>
   </el-container>
 
@@ -60,9 +105,73 @@
       };
 
       this.getApiData();
+      this.getShopData();
 
       return {
-        tableData: Array(3).fill(item),
+        // shopData: [{
+        //   id: 1,
+        //   pid: "000_73",
+        //   name: "HYShop_name8",
+        //   price: 6,
+        //   des: "HYShop_des4",
+        //   add_date: 1526985868,
+        //   update_time: 1526985868
+        // },
+        //   {
+        //     id: 2,
+        //     pid: "000_59",
+        //     name: "HYShop_name9",
+        //     price: 6,
+        //     des: "HYShop_des6",
+        //     add_date: 1526985877,
+        //     update_time: 1526985877
+        //   },
+        //   {
+        //     id: 3,
+        //     pid: "000_61",
+        //     name: "HYShop_name4",
+        //     price: 5,
+        //     des: "HYShop_des1",
+        //     add_date: 1526985877,
+        //     update_time: 1526985877
+        //   },
+        //   {
+        //     id: 4,
+        //     pid: "000_4",
+        //     name: "HYShop_name5",
+        //     price: 8,
+        //     des: "HYShop_des3",
+        //     add_date: 1526985878,
+        //     update_time: 1526985878
+        //   },
+        //   {
+        //     id: 5,
+        //     pid: "000_67",
+        //     name: "HYShop_name6",
+        //     price: 4,
+        //     des: "HYShop_des2",
+        //     add_date: 1526985878,
+        //     update_time: 1526985878
+        //   }],
+        shopData: [],
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        // tableData: Array(3).fill(item),
         movieData: [],
         activeIndex: '1',
         activeIndex2: '1',
@@ -94,6 +203,24 @@
           that.movieData = result.data.result;
           that.loading2 = false;
         });
+      },
+      getShopData: function () {
+        var url = "/zkteam/Shop/jsonShow";
+        var that = this;
+        that.loading2 = true;
+
+
+        this.$axios.get(url).then(function (result) {
+          console.log(result);
+          that.shopData = result.data.result;
+          that.loading2 = false;
+        });
+      },
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
       }
     }
   }
